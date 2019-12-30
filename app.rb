c5cfdd4b1337d8b3420dad63783791412750855c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-30 03:37:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  
+  # get '/hey' do 
+  #   session["name"] = "Victoria"
+  #   @session = session
+  # end
+
 end
